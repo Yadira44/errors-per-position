45e6f28889cfd5d3327c98c83e956b131db7ee10
Yadira44 <pugay747@gmail.com>
2025-04-23 21:30:09 -0500

    Create Position_total_errors.R

---
+++ Position_total_errors.R
@@ -0,0 +1,72 @@
+# Part 1: Find Total 2019 Errors Type
+
+error_2019_rows_filtered$e1_sum <- sum(error_2019_rows_filtered$e1, na.rm = TRUE)
+error_2019_rows_filtered$e2_sum <- sum(error_2019_rows_filtered$e2, na.rm = TRUE)
+error_2019_rows_filtered$e3_sum <- sum(error_2019_rows_filtered$e3, na.rm = TRUE)
+error_2019_rows_filtered$e4_sum <- sum(error_2019_rows_filtered$e4, na.rm = TRUE)
+error_2019_rows_filtered$e5_sum <- sum(error_2019_rows_filtered$e5, na.rm = TRUE)
+error_2019_rows_filtered$e6_sum <- sum(error_2019_rows_filtered$e6, na.rm = TRUE)
+error_2019_rows_filtered$e7_sum <- sum(error_2019_rows_filtered$e7, na.rm = TRUE)
+error_2019_rows_filtered$e8_sum <- sum(error_2019_rows_filtered$e8, na.rm = TRUE)
+error_2019_rows_filtered$e9_sum <- sum(error_2019_rows_filtered$e9, na.rm = TRUE)
+
+# Part 2: Find Total 2020 Errors Type
+
+error_2020_rows_filtered$e1_sum <- sum(error_2020_rows_filtered$e1, na.rm = TRUE)
+error_2020_rows_filtered$e2_sum <- sum(error_2020_rows_filtered$e2, na.rm = TRUE)
+error_2020_rows_filtered$e3_sum <- sum(error_2020_rows_filtered$e3, na.rm = TRUE)
+error_2020_rows_filtered$e4_sum <- sum(error_2020_rows_filtered$e4, na.rm = TRUE)
+error_2020_rows_filtered$e5_sum <- sum(error_2020_rows_filtered$e5, na.rm = TRUE)
+error_2020_rows_filtered$e6_sum <- sum(error_2020_rows_filtered$e6, na.rm = TRUE)
+error_2020_rows_filtered$e7_sum <- sum(error_2020_rows_filtered$e7, na.rm = TRUE)
+error_2020_rows_filtered$e8_sum <- sum(error_2020_rows_filtered$e8, na.rm = TRUE)
+error_2020_rows_filtered$e9_sum <- sum(error_2020_rows_filtered$e9, na.rm = TRUE)
+
+# Part 2: Find Total 2021 Errors Type
+
+error_2021_rows_filtered$e1_sum <- sum(error_2021_rows_filtered$e1, na.rm = TRUE)
+error_2021_rows_filtered$e2_sum <- sum(error_2021_rows_filtered$e2, na.rm = TRUE)
+error_2021_rows_filtered$e3_sum <- sum(error_2021_rows_filtered$e3, na.rm = TRUE)
+error_2021_rows_filtered$e4_sum <- sum(error_2021_rows_filtered$e4, na.rm = TRUE)
+error_2021_rows_filtered$e5_sum <- sum(error_2021_rows_filtered$e5, na.rm = TRUE)
+error_2021_rows_filtered$e6_sum <- sum(error_2021_rows_filtered$e6, na.rm = TRUE)
+error_2021_rows_filtered$e7_sum <- sum(error_2021_rows_filtered$e7, na.rm = TRUE)
+error_2021_rows_filtered$e8_sum <- sum(error_2021_rows_filtered$e8, na.rm = TRUE)
+error_2021_rows_filtered$e9_sum <- sum(error_2021_rows_filtered$e9, na.rm = TRUE)
+
+
+# Part 2: Find Total 2022 Errors Type
+
+error_2022_rows_filtered$e1_sum <- sum(error_2022_rows_filtered$e1, na.rm = TRUE)
+error_2022_rows_filtered$e2_sum <- sum(error_2022_rows_filtered$e2, na.rm = TRUE)
+error_2022_rows_filtered$e3_sum <- sum(error_2022_rows_filtered$e3, na.rm = TRUE)
+error_2022_rows_filtered$e4_sum <- sum(error_2022_rows_filtered$e4, na.rm = TRUE)
+error_2022_rows_filtered$e5_sum <- sum(error_2022_rows_filtered$e5, na.rm = TRUE)
+error_2022_rows_filtered$e6_sum <- sum(error_2022_rows_filtered$e6, na.rm = TRUE)
+error_2022_rows_filtered$e7_sum <- sum(error_2022_rows_filtered$e7, na.rm = TRUE)
+error_2022_rows_filtered$e8_sum <- sum(error_2022_rows_filtered$e8, na.rm = TRUE)
+error_2022_rows_filtered$e9_sum <- sum(error_2022_rows_filtered$e9, na.rm = TRUE)
+
+# Part 2: Find Total 2023 Errors Type
+
+error_2023_rows_filtered$e1_sum <- sum(error_2023_rows_filtered$e1, na.rm = TRUE)
+error_2023_rows_filtered$e2_sum <- sum(error_2023_rows_filtered$e2, na.rm = TRUE)
+error_2023_rows_filtered$e3_sum <- sum(error_2023_rows_filtered$e3, na.rm = TRUE)
+error_2023_rows_filtered$e4_sum <- sum(error_2023_rows_filtered$e4, na.rm = TRUE)
+error_2023_rows_filtered$e5_sum <- sum(error_2023_rows_filtered$e5, na.rm = TRUE)
+error_2023_rows_filtered$e6_sum <- sum(error_2023_rows_filtered$e6, na.rm = TRUE)
+error_2023_rows_filtered$e7_sum <- sum(error_2023_rows_filtered$e7, na.rm = TRUE)
+error_2023_rows_filtered$e8_sum <- sum(error_2023_rows_filtered$e8, na.rm = TRUE)
+error_2023_rows_filtered$e9_sum <- sum(error_2023_rows_filtered$e9, na.rm = TRUE)
+
+# Part 2: Find Total 2024 Errors Type
+
+error_2024_rows_filtered$e1_sum <- sum(error_2024_rows_filtered$e1, na.rm = TRUE)
+error_2024_rows_filtered$e2_sum <- sum(error_2024_rows_filtered$e2, na.rm = TRUE)
+error_2024_rows_filtered$e3_sum <- sum(error_2024_rows_filtered$e3, na.rm = TRUE)
+error_2024_rows_filtered$e4_sum <- sum(error_2024_rows_filtered$e4, na.rm = TRUE)
+error_2024_rows_filtered$e5_sum <- sum(error_2024_rows_filtered$e5, na.rm = TRUE)
+error_2024_rows_filtered$e6_sum <- sum(error_2024_rows_filtered$e6, na.rm = TRUE)
+error_2024_rows_filtered$e7_sum <- sum(error_2024_rows_filtered$e7, na.rm = TRUE)
+error_2024_rows_filtered$e8_sum <- sum(error_2024_rows_filtered$e8, na.rm = TRUE)
+error_2024_rows_filtered$e9_sum <- sum(error_2024_rows_filtered$e9, na.rm = TRUE)
